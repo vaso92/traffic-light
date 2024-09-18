ea5e9a70efe3808f82b67b484f2a5f7d1950cc7c
Vasil Vasilev <vasil.vasilev@concordeu.com>
2024-09-18 14:20:30 +0300

    Initial commit with an empty activity.

---
+++ app/src/main/java/com/vasilv/traffic_light/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.vasilv.traffic_light
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vasilv.traffic_light.ui.theme.TrafficlightTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TrafficlightTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
